41766b75f1f310d996df69a8c13ecddc9399382c
Timotius Wirawan <60548098+timmtimm@users.noreply.github.com>
2021-03-27 12:45:24 +0700

    Update soal1.sh

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -3,12 +3,12 @@
 #  nomor 1 a
 grep -oE "(INFO.*)|(ERROR.*)" syslog.log
 
-#  nomor 1 b
+#   nomor 1 b
 grep -oE 'ERROR.*' syslog.log
 echo | grep -cE 'ERROR' syslog.log 
 
-#   nomor 1 c
-name=`grep -oP "(\([a-zA-Z]+\))" syslog.log | sort | uniq | grep -oP "(?<=\().*(?=\))"`
+  nomor 1 c
+name=`grep -oE "(\([a-zA-Z]+\))" syslog.log | sort | uniq | grep -oP "(?<=\().*(?=\))"`
 echo "$name" |
 while read -r lines
 do
@@ -30,7 +30,7 @@ done | sort  -rnk 2 -t ',' >> error_message.csv
 
 # nomor 1 e
 echo "Username", "INFO", "ERROR" > user_statistics.csv
-name=`grep -oP "(\([a-zA-Z]+\))" syslog.log | sort | uniq | grep -oP "(?<=\().*(?=\))"`
+name=`grep -oE "(\([a-zA-Z]+\))" syslog.log | sort | uniq | grep -oP "(?<=\().*(?=\))"`
 for i in $name
 do
     echo "$i, $(grep -cP "INFO.*($i)" syslog.log), $(grep -cP "ERROR.*($i)" syslog.log)"
